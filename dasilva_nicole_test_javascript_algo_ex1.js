6b37128b234325ec00ac6a2075d7a02c47452818
nikingrid <nikpam2008@hotmail.com>
2022-09-27 17:57:08 +0200

    creada tabla 9x9

--- dasilva_nicole_test_javascript_algo_ex1.js
+++ dasilva_nicole_test_javascript_algo_ex1.js
@@ -1,1 +1,15 @@
 
+function crearTabla(){ 
+    let filas = parseInt(prompt('numero filas'));
+    let columnas = parseInt(prompt('numero columnas'));
+    
+    for (let i=0; i< filas; i++){
+        let filaActual = document.getElementById('tablaArray').insertRow(i);
+    }
+    
+    for (let j=0; j< columnas; j++){
+        let celda = filaActual.insertCell(j);
+        
+        celda.innerHTML = `Fila: ${i} Columna: ${j} `;
+    }
+}
